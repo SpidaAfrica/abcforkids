41f179a776fa04d6e6fb963b55c90925cea554b9
SpidaAfrica <spidaafrica@gmail.com>
2025-08-26 13:06:31 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,22 @@
-const CACHE_NAME = 'abc-store-v1';
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/script.js',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+const CACHE_NAME = "abc-kids-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png"
 ];
 
-// Install and cache files
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// Activate and clear old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Fetch from cache, fallback to network
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
